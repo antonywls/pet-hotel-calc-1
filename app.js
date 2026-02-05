9aad1b452801c1bf02244296db3bb937fce2fa8b
Antony Wilson <wilsondantony@gmail.com>
2026-02-06 02:31:46 +1000

    Add input test fields

--- app.js
+++ app.js
@@ -3,6 +3,14 @@ const result = document.getElementById("result");
 
 helloBtn.addEventListener("click", () => {
   const msg = "Hello world — JS is working.";
-  alert(msg);
+  //alert(msg);
   result.textContent = msg;
+});
+
+helloBtn.addEventListener("mouseenter", () => {
+  result.textContent = "you are hovering";
+});
+
+helloBtn.addEventListener("mouseleave", () => {
+  result.textContent = "";
 });
